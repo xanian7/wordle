c9be4bf5649019fbf85f044ace5ae1491318c2fe
xanian7 <xanian407@gmail.com>
2024-10-13 20:46:14 -0400

    dino game rawr

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,13 @@ const router = createRouter({
     {
       path: '/wordle',
       name: 'wordle',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/WordleView.vue')
-    }
+    },
+    {
+      path: '/dinogame',
+      name: 'dinogame',
+      component: () => import('../views/DinoGameView.vue')
+    },
   ]
 })
 
---
+++ src/views/DinoGameView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <DinoPlayer></DinoPlayer>
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from 'vue';
+import DinoPlayer from '../components/Dino.vue'
+
+export default defineComponent({
+    name: 'DinoGame',
+    components: {
+        DinoPlayer,
+    },
+    setup() {
+    }
+})
+</script>
+
+<style>
+
+</style>
