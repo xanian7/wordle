978de07021b7f38bad758380b94a28921160ffa4
xanian7 <xanian407@gmail.com>
2024-10-28 16:10:28 -0400

    changing base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wordle/',
+  base: '/x.kraem.com/',
   plugins: [
     vue(),
     vueDevTools(),
