4bec43fba0e520860dc50dd3be77075b8c66926d
xanian7 <xanian407@gmail.com>
2024-10-28 15:43:25 -0400

    adjusting for space on mobile

--- src/views/WordleView.vue
+++ src/views/WordleView.vue
@@ -111,7 +111,7 @@ export default {
     const keyboardRows = ref([
       ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
       ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-      ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Backspace"],
+      ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Back"],
     ]);
 
     const theWord = ref("");
