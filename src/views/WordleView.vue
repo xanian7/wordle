937f658f45f0031628f94c727e5fb89fcf644546
xanian7 <xanian407@gmail.com>
2024-10-28 15:50:32 -0400

    disable input from keyboard

--- src/views/WordleView.vue
+++ src/views/WordleView.vue
@@ -12,7 +12,7 @@
       >
         <div class="letter-box-inner">
           <div class="letter-box-front">
-            <input
+            <input disabled
               v-if="rowIndex === currentRow"
               v-model="grid[rowIndex][colIndex]"
               maxlength="1"
@@ -111,7 +111,7 @@ export default {
     const keyboardRows = ref([
       ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
       ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-      ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Back"],
+      ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Backspace"],
     ]);
 
     const theWord = ref("");
